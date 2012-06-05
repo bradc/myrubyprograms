599cbfbc5827a8483ead35a1ee9e25558b1dd9b4
Brad Coish <brad.coish@gmail.com>
2012-06-05 01:11:25 -0300

    Week 1 exercise answers
    Using RSpec for tests is a bit much for week 1, but the contents of the
    tests should be grokable by most users.

---
+++ Week1/temp_convert.rb
@@ -0,0 +1,12 @@
+# Temp_Convert
+# Converts Fahrenheit temperature to Celsius.
+# Brad
+# Batch 33 Rubylearning Week 1, Ex. 6
+#
+#[°C] = ([°F] - 32) × 5/9
+#[°F] = [°C] × 9/5 + 32 
+# 
+def convert f_temp
+	celsius = (f_temp - 32) * 5/9.0
+end
+
